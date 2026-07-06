1dde1c85903cf63dcd6b5c47cfa4a1d1b4304552
Parth Bindal <parthbindal07@gmail.com>
2026-07-06 20:41:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (70.27%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double ans = pow(x,n);
+        long binform = n;
+        double ans = 1;
+
+        if (n < 0) {
+            binform = -binform;
+            x = 1/x;
+        }
+
+        while (binform > 0) {
+            if (binform % 2 == 1) {
+                ans *= x;
+            }
+            x *= x;
+            binform /= 2;
+        }
         return ans;
     }
 };
